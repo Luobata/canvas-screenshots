765b000fd77733dcdd39f7347fb2d147f678064c
luobata <batayao@sohu-inc.com>
2017-11-28 10:55:58 +0800

    add-eslint

--- __tests__/app.js
+++ __tests__/app.js
@@ -14,6 +14,7 @@ describe('generator-luobata-code:app', () => {
       '.babelrc',
       '.gitignore',
       '.tern-project',
+      '.eslintrc',
       'package.json',
       'src/index.js',
       'dist',
