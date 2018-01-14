d221f55c77488ff09488369bd8e1799571d24b27
Luobata <602975805@qq.com>
2018-01-14 17:31:29 +0800

    upd8: framework;
    Signed-off-by: Luobata <602975805@qq.com>

--- src/canvas-framework/ui/Canvas.ts
+++ src/canvas-framework/ui/Canvas.ts
@@ -1,9 +1,19 @@
+import { setConfig } from '../util/config';
+
 export default class Canvas {
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
     width: number;
     height: number;
-    constructor() {}
+    constructor(width: number, height: number, canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+        this.width = width;
+        this.height = height;
+    }
 
+    resize(size: { width?: number; height?: number }) {
+        Object.assign(this, size);
+    }
     draw() {}
 }
