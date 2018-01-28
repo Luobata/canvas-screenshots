7a182d4fbb374783978c3c3dd047183e2e2f9bbe
Luobata <602975805@qq.com>
2018-01-29 01:15:58 +0800

    add: arrow draw();
    Signed-off-by: Luobata <602975805@qq.com>

--- src/canvas/insert/arrow.ts
+++ src/canvas/insert/arrow.ts
@@ -76,22 +76,37 @@ export default class {
             Math.pow(this.rect.endX - this.rect.startX, 2) +
                 Math.pow(this.rect.endY - this.rect.startY, 2),
         );
-        const arrowWid = lineWid / 10; // 箭头位置总长度的十分之一
+        const arrowWid = lineWid * 0.2; // 箭头位置总长度的十分之一
+        const arrowInWid = arrowWid * 0.7;
+        const rec = Math.atan(
+            Math.abs(this.rect.endY - this.rect.startY) /
+                Math.abs(this.rect.endX - this.rect.startX),
+        );
         const P1 = {
-            x: this.rect.endX - arrowWid * Math.sin(Math.PI * 3 / 8),
-            y: this.rect.endY - arrowWid * Math.cos(Math.PI * 3 / 8),
+            x: this.rect.endX - arrowWid * Math.cos(rec / 2 * 3),
+            y: this.rect.endY - arrowWid * Math.sin(rec / 2 * 3),
         };
         const P2 = {
-            x: this.rect.endX - arrowWid * Math.sin(Math.PI * 1 / 8),
-            y: this.rect.endY - arrowWid * Math.cos(Math.PI * 1 / 8),
+            x: this.rect.endX - arrowWid * Math.cos(rec / 2 * 1),
+            y: this.rect.endY - arrowWid * Math.sin(rec / 2 * 1),
+        };
+        const P3 = {
+            x: this.rect.endX - arrowInWid * Math.cos(rec / 2 * 3 - rec / 4),
+            y: this.rect.endY - arrowInWid * Math.sin(rec / 2 * 3 - rec / 4),
+        };
+        const P4 = {
+            x: this.rect.endX - arrowInWid * Math.cos(rec / 2 * 1 + rec / 4),
+            y: this.rect.endY - arrowInWid * Math.sin(rec / 2 * 1 + rec / 4),
         };
         this.ctx.save();
         this.ctx.beginPath();
         this.ctx.fillStyle = this.color;
-        this.ctx.lineTo(this.rect.startX, this.rect.startY);
-        this.ctx.moveTo(P1.x, P1.y);
-        this.ctx.moveTo(this.rect.endX, this.rect.endY);
-        this.ctx.moveTo(P2.x, P1.y);
+        this.ctx.moveTo(this.rect.startX, this.rect.startY);
+        this.ctx.lineTo(P3.x, P3.y);
+        this.ctx.lineTo(P1.x, P1.y);
+        this.ctx.lineTo(this.rect.endX, this.rect.endY);
+        this.ctx.lineTo(P2.x, P2.y);
+        this.ctx.lineTo(P4.x, P4.y);
         this.ctx.fill();
         this.ctx.restore();
     }
